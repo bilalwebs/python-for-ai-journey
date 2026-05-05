158f4252d68a1b36bcdc15be1e9fdb605545130d
bilalwebs <bil2023aljutt@gmail.com>
2026-05-05 16:19:42 +0500

    while_loop

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+
+
+# counter = 1  # intialization
+
+# while counter < 6:  # condition
+#     print(counter)
+#     counter += 1  # update
+
+
+# answer = " "
+# while answer != "yes":
+#     answer = input("Do you agree(yes/no): ")
+# print("thnk you")
+
+
+while True:
+    answer = input("Do you agree(yes/no): ")
+    if answer == "yes":
+        break
+print("thanks")
+

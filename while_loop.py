b2ec58fd2caf1b9a1808165906d309db25e8c439
bilalwebs <bil2023aljutt@gmail.com>
2026-05-06 16:07:16 +0500

    challange solve

--- while_loop.py
+++ while_loop.py
@@ -13,9 +13,75 @@
 # print("thnk you")
 
 
-while True:
-    answer = input("Do you agree(yes/no): ")
-    if answer == "yes":
-        break
-print("thanks")
+# while True:
+#     answer = input("Do you agree(yes/no): ")
+#     if answer == "yes":
+#         break
+# print("thanks")
 
+
+# ! challenge
+
+# * 1. Challenge Instructions:
+
+# Allow up to 3 attempts
+
+# If the user types "yes", print "Glad we are on the same page"
+
+# Otherwise, print "3 Strikes, You are Out!"
+
+
+# attempt = 0
+
+# while attempt < 3:
+#     answer = input("Do you agree? (yes/no): ")
+#     if answer == "yes":
+#         print("Glad we are on the same page")
+#         break
+#     attempt += 1
+
+# if attempt == 3:
+#     print("3 Strikes, You are Out!")
+
+
+# *2. Challenge: The Password Protector (Basic)
+# password = " "
+# while password != "python123":
+#     password = input("Enter your Password: ")
+#     if password != "python123":
+#         print("Wrong Passwor: Try again")
+
+# print("Congratulation")
+
+
+# *3. Challenge: Number Guess
+# secret_num = 7
+# attempts = 0
+
+# while attempts < 5:
+#     user_gucess = int(input("Guess the number: "))
+
+#     # 1. Compare with secret_num, NOT attempts
+#     if user_gucess == secret_num:
+#         print("Winner! You guessed it.")
+#         break
+#     else:
+#         attempts += 1
+#         print("Wrong guess! Try again.")
+
+# # 2. Check if the loop finished because attempts ran out
+# if attempts == 5:
+#     print("Game Over! You've used all 5 attempts.")
+
+
+# * 4
+
+num = 1
+
+while num < 11:
+    if num % 2 == 0:
+        print("Even Number")
+    else:
+        print("Odd numver")
+
+    num += 1
